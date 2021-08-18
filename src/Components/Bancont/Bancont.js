072cf50e989354661e86871bdcf88b02b3a27fdd
Arunkumar-S <89118161+Arunkumar-S@users.noreply.github.com>
2021-08-18 09:53:29 +0530

    Add files via upload

---
+++ src/Components/Bancont/Bancont.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Bancont.css'
+const Bancont = () => {
+    return (
+        <div className="ban">
+            <div class="bancont">
+            <div className="ban-heading">
+                <h1>Your <span>Technology Partner</span></h1>
+            </div>
+            <div className="ban-content">
+                <p>We are a custom software development company focused on helping startups and mid-size businesses reach their full potential by building great websites and intuitive mobile apps.</p>
+            </div>
+            <div className="ban-content1">
+                <p>Why choose us? Because we care. Your every detail and requirement will be discussed and solved in the way you want. Our processes are transparent – you will always know what we are working on, and why. With us it is a real win-win – high-quality software at affordable prices.</p>
+            </div>
+            <div className="ban-content-bold">
+                <p>Looking for a reliable partner? Don’t look any further.</p>
+            </div>
+          <button className="ban-content-button">Get Started</button>
+            </div>
+        </div>
+    )
+}
+
+export default Bancont
